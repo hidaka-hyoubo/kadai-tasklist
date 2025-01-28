12b665a77151cb4eefef488f8c48093e16812157
tsubasa.hidaka <hidaka@hyoubo.com>
2025-01-28 07:15:17 +0000

    correct migrations

--- database/migrations/2025_01_28_115945_add_title_to_tasks_table.php
+++ database/migrations/2025_01_28_115945_add_title_to_tasks_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status',10);
+            $table->dropColumn('status',10);
         });
     }
 };
